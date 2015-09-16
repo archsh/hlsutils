c3dc63bfb718f906af61bff6d7cb927738e11f1b
Mingcai SHEN <archsh@gmail.com>
2015-09-16 18:43:50 +0800

    Updated.

--- src/hls-sync/hls-sync.go
+++ src/hls-sync/hls-sync.go
@@ -18,6 +18,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
@@ -56,29 +57,34 @@ type Download struct {
 func getsaveSegment(url string, filename string) (string, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		// log.Fatal(err)
 	}
 
 	err = os.MkdirAll(path.Dir(filename), 0777)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		// log.Fatal(err)
 	}
 
 	out, err := os.Create(filename)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		// log.Fatal(err)
 	}
 	defer out.Close()
 	resp, err := doRequest(client, req)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		// log.Fatal(err)
 	}
 	if resp.StatusCode != 200 {
 		log.Printf("Received HTTP %v for %v \n", resp.StatusCode, url)
 	}
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		// log.Fatal(err)
 	}
 	resp.Body.Close()
 	log.Printf("Downloaded %v into %v\n", url, filename)
@@ -89,7 +95,8 @@ func downloadSegment(dlc chan *Download, recTime time.Duration) {
 	for v := range dlc {
 		_, err := getsaveSegment(v.URI, v.Filename)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			// log.Fatal(err)
 		}
 		if recTime != 0 {
 			log.Printf("Recorded %v of %v\n", v.totalDuration, recTime)
@@ -107,28 +114,37 @@ func deleteOldSegment(filename string) {
 	}
 }
 
-func getPlaylist(urlStr string, outDir string, recTime time.Duration, deleteOld bool, useLocalTime bool, dlc chan *Download) {
+func getPlaylist(urlStr string, outDir string, recTime time.Duration, deleteOld bool, useLocalTime bool, retry int, dlc chan *Download) {
 	startTime := time.Now()
 	var recDuration time.Duration = 0
 	var firstList = true
 	cache := lru.New(1024)
-	cache.OnEvicted = func(key lru.Key, value interface{}) {
-		fname, res := value.(string)
-		if res {
-			deleteOldSegment(fname)
+	if deleteOld {
+		cache.OnEvicted = func(key lru.Key, value interface{}) {
+			fname, res := value.(string)
+			if res {
+				deleteOldSegment(fname)
+			}
 		}
 	}
+
 	outPath, err := os.Open(outDir)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		// log.Fatal(err)
+		return
 	}
 	defer outPath.Close()
 	fstat, err := outPath.Stat()
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		// log.Fatal(err)
+		return
 	}
 	if fstat.IsDir() != true {
-		log.Fatal("Output is not a directory!")
+		log.Println("Output is not a directory!")
+		// log.Fatal("Output is not a directory!")
+		return
 	}
 
 	//	playlistUrl, err := url.Parse(urlStr)
@@ -139,27 +155,45 @@ func getPlaylist(urlStr string, outDir string, recTime time.Duration, deleteOld 
 	for {
 		req, err := http.NewRequest("GET", urlStr, nil)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			// log.Fatal(err)
+			return
 		}
 		resp, err := doRequest(client, req)
 		if err != nil {
-			log.Print(err)
+			log.Println(err)
 			time.Sleep(time.Duration(3) * time.Second)
+			continue
 		}
 		respBody, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			// log.Fatal(err)
+			time.Sleep(time.Duration(3) * time.Second)
+			continue
 		}
 		buffer := bytes.NewBuffer(respBody)
 		playlistFilename := path.Join(outDir, resp.Request.URL.Path)
 		err = os.MkdirAll(path.Dir(playlistFilename), 0777)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			// log.Fatal(err)
+			return
 		}
 		playlist, listType, err := m3u8.Decode(*buffer, true)
 		//		m3u8.DecodeFrom(resp.Body, true)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			// log.Fatal(err)
+			time.Sleep(time.Duration(3) * time.Second)
+			if retry < 0 {
+				continue
+			} else if retry > 0 {
+				retry--
+				continue
+			} else {
+				return
+			}
 		}
 		resp.Body.Close()
 		if listType == m3u8.MEDIA {
@@ -171,7 +205,8 @@ func getPlaylist(urlStr string, outDir string, recTime time.Duration, deleteOld 
 					if strings.HasPrefix(v.URI, "http") {
 						msURI, err = url.QueryUnescape(v.URI)
 						if err != nil {
-							log.Fatal(err)
+							log.Println(err)
+							// log.Fatal(err)
 						}
 						msFilename = path.Join(path.Dir(playlistFilename), path.Base(msURI))
 					} else {
@@ -182,7 +217,8 @@ func getPlaylist(urlStr string, outDir string, recTime time.Duration, deleteOld 
 						}
 						msURI, err = url.QueryUnescape(msUrl.String())
 						if err != nil {
-							log.Fatal(err)
+							log.Println(err)
+							// log.Fatal(err)
 						}
 						msFilename = path.Join(outDir, msUrl.Path)
 					}
@@ -204,11 +240,12 @@ func getPlaylist(urlStr string, outDir string, recTime time.Duration, deleteOld 
 			}
 			out, err := os.Create(playlistFilename)
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				// log.Fatal(err)
 			}
 			io.Copy(out, buffer)
 			out.Close()
-			if deleteOld && !firstList {
+			if !firstList {
 				cache.RemoveOldest()
 			}
 			firstList = false
@@ -219,7 +256,15 @@ func getPlaylist(urlStr string, outDir string, recTime time.Duration, deleteOld 
 				time.Sleep(time.Duration(int64(mpl.TargetDuration * 1000000000)))
 			}
 		} else {
-			log.Fatal("Not a valid media playlist")
+			log.Println("Not a valid media playlist")
+			if retry < 0 {
+				continue
+			} else if retry > 0 {
+				retry--
+				continue
+			} else {
+				return
+			}
 		}
 	}
 }
@@ -229,17 +274,20 @@ func main() {
 	duration := flag.Duration("t", time.Duration(0), "Recording duration (0 == infinite)")
 	useLocalTime := flag.Bool("l", false, "Use local time to track duration instead of supplied metadata")
 	deleteOld := flag.Bool("d", false, "Delete old segments.")
+	retryTimes := flag.Int("r", 0, "Retry if failed. 0: Never retry, <0: Keep retry, >0: Retry times.")
 	var outputDir string
 	flag.StringVar(&outputDir, "o", ".", "Output path. default is '.'")
+	var listFilename string
+	flag.StringVar(&listFilename, "u", "", "URL list file for multiple sync.")
 	flag.StringVar(&USER_AGENT, "ua", fmt.Sprintf("hls-sync/%v", VERSION), "User-Agent for HTTP Client")
 	flag.Parse()
-	
+
 	endChan := make(chan bool)
 
 	os.Stderr.Write([]byte(fmt.Sprintf("hls-sync %v - HTTP Live Streaming (HLS) Synchronizar\n", VERSION)))
 	os.Stderr.Write([]byte("Copyright (C) 2015 Mingcai SHEN. Licensed for use under the GNU GPL version 3.\n"))
 
-	if flag.NArg() < 1 {
+	if flag.NArg() < 1 && listFilename == "" {
 		os.Stderr.Write([]byte("Usage: hls-sync [Options] media-playlist-url output-path\n"))
 		os.Stderr.Write([]byte("Options:\n"))
 		flag.PrintDefaults()
@@ -247,12 +295,31 @@ func main() {
 		os.Exit(2)
 	}
 
-	for _, link := range flag.Args() {
+	var linkList []string
+
+	if listFilename != "" {
+		f, e := os.Open(listFilename)
+		if e != nil {
+			fmt.Printf("Open file %s failed! [%v]\n", listFilename, e)
+			os.Exit(2)
+		}
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			linkList = append(linkList, scanner.Text())
+		}
+	} else {
+		linkList = flag.Args()
+	}
+
+	for _, link := range linkList {
+		log.Printf("Start to get link: %s \n", link)
 		if !strings.HasPrefix(link, "http") {
-			log.Fatal("Media playlist url must begin with http/https")
+			fmt.Println("Media playlist url must begin with http/https")
+			os.Exit(2)
 		}
 		msChan := make(chan *Download, 1024)
-		go getPlaylist(link, outputDir, *duration, *deleteOld, *useLocalTime, msChan)
+		go getPlaylist(link, outputDir, *duration, *deleteOld, *useLocalTime, *retryTimes, msChan)
 		go downloadSegment(msChan, *duration)
 	}
 
@@ -260,10 +327,10 @@ func main() {
 	signal.Notify(term_c, os.Interrupt)
 	for {
 		select {
-		case <- term_c:
+		case <-term_c:
 			log.Printf("User controled terminated.\n")
 			os.Exit(0)
-		case <- endChan:
+		case <-endChan:
 			log.Printf("Sync finished!\n")
 			break
 		}
